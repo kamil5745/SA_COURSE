e2d2831612585cee52606ec7e5ce53fa5c6758d4
kamil5745 <kamilmingaliev0@gmail.com>
2026-06-29 19:46:08 +0300

    Перед клоинрованием

--- src/schemas.py
+++ src/schemas.py
@@ -2,9 +2,30 @@ from datetime import datetime
 from typing import Optional
 from pydantic import BaseModel, ConfigDict
 
+from models import Workload
 
+class WorkersAddDTO(BaseModel):
+    username: str
 
+class WorkersDTO(WorkersAddDTO):
+    id: int
 
+class ResumesAddDTO(BaseModel):
+    title: str
+    compesation: Optional[int]
+    worklaod: Workload
+    worker_id: int
+
+class ResumesDTO(ResumesAddDTO):
+    id: int
+    created_at: datetime
+    update_at: datetime
+
+class ResumesRelDTO(ResumesDTO):
+    worker: "WorkersDTO"
+
+class WorkersRelDTO(WorkersDTO):
+    resumes: list["ResumesDTO"]
 
 
 
